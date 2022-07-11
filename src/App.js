efc146b90163671bec419c77de55887c3d642847
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-11 21:21:25 +0700

    Add: Auto Format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default () => {
+  const testAnd = false;
+  let numBox = 0;
   return (
-    <div>Emmet pass</div>
-  )
-}
+    <div>
+      <div>Emmet pass</div>
+      <div>{numBox}</div>
+      {testAnd ? <div>yes</div> : <div>no</div>}
+      <div>{testAnd && "Yes and"}</div>
+      <button
+        onClick={() => {
+          numBox = 10;
+        }}
+      >
+        Change
+      </button>
+    </div>
+  );
+};
