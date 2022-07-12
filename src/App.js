85fc5a51a09ad19a0d021903755eb367ad593547
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-13 01:39:47 +0700

    add: style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-
+import './components/styleComponents/style.css'
 export default () => {
-  const testAnd = false;
   let numBox = 0;
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>Count : {count} </h1>
-      <button
-        onClick={() => {
-					setCount(count + 1)
-			}}
-      >
-        Change
-      </button>
+      <div className="container">
+        <h1>Count : {count} </h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Change
+        </button>
+      </div>
     </div>
   );
 };
