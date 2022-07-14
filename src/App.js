48d188c96a0f2333e27505ff39beaf5fa90ff253
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-14 18:37:34 +0700

    clean: variable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import HelloComponent from "./components/HelloComponent";
 import "./components/styleComponents/style.css";
 export default () => {
-  let numBox = 0;
   const [count, setCount] = useState(0);
 
   return (
--- src/components/HelloComponent.js
+++ src/components/HelloComponent.js
@@ -1,14 +1,14 @@
 import React from "react";
-import propTypes from "prop-types"
+import propTypes from "prop-types";
 export default function HelloComponent(props) {
   return (
-	<div>
-		{props.img && <img src={props.img} alt="" />}
-		<h1>Count : {props.count} </h1>
-	</div>
-	);
+    <div>
+      {props.img && <img src={props.img} alt="" />}
+      <h1>Count : {props.count} </h1>
+    </div>
+  );
 }
 
 HelloComponent.propTypes = {
-	count: propTypes.number
-}
+  count: propTypes.number,
+};
