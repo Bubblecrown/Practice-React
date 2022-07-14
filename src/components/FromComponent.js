d6812a552f17250c37e1a1a48801f2ce97e3b358
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-15 02:08:14 +0700

    add: map-display

--- src/components/FromComponent.js
+++ src/components/FromComponent.js
@@ -3,32 +3,34 @@ import React, { useState } from "react";
 export default function FromComponent() {
   const initialState = { username: "", password: "" };
   const [logIn, setlogIn] = useState(initialState);
+  const [account, setAccount] = useState([]);
   return (
     <div>
-			<p>Debug {JSON.stringify(logIn)}</p>
+      <p>Debug {JSON.stringify(logIn)}</p>
       <form action="">
         <input
           type="text"
           placeholder="Username"
-					value={logIn.username}
+          value={logIn.username}
           onChange={(e) => {
-            setlogIn({...logIn, username: e.target.value });
+            setlogIn({ ...logIn, username: e.target.value });
           }}
         />
         <br />
         <input
           type="password"
           placeholder="Password"
-					value={logIn.password}
+          value={logIn.password}
           onChange={(e) => {
-            setlogIn({...logIn, password: e.target.value});
+            setlogIn({ ...logIn, password: e.target.value });
           }}
         />
         <br />
         <button
           onClick={(e) => {
             e.preventDefault();
-						alert(JSON.stringify(logIn))
+            setAccount([...account, { ...logIn, index: account.length }]);
+            // alert(JSON.stringify(logIn));
           }}
         >
           Submit
@@ -37,12 +39,21 @@ export default function FromComponent() {
         <button
           onClick={(e) => {
             e.preventDefault();
-						setlogIn(initialState)
+            setlogIn(initialState);
           }}
         >
           Clear
         </button>
       </form>
+      <br />
+
+      <ul>
+        {account.map(
+          (user) => (
+            (<li key={user.index}>Username: {user.username} <br /> Password: {user.password}</li>)
+          )
+        )}
+      </ul>
     </div>
   );
 }
