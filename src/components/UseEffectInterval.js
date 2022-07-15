6421d9d9a9bb344b58605f4cabf64765da10fa09
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-16 03:35:41 +0700

    edit: useEffect

--- src/components/UseEffectInterval.js
+++ src/components/UseEffectInterval.js
@@ -3,15 +3,17 @@ import React, { useEffect, useState } from "react";
 export default function UseEffectInterval() {
 	const [time, setTime] = useState(0)
   const timer = () => {
-    setTime(time + 1);
+    setTime(previous => previous + 1);
+		console.log("state")
   };
   useEffect(() => {
     const countTime = setInterval(timer, 1000);
 
     return () => {
       clearInterval(countTime);
+			console.log("Destroyed")
     };
-  }, [time]); // useState รันอยู่ตลอดแต่ display แสดงผลไม่ได้รับการอัปเดตถ้าไม่ได้ใช้ useEffect
+  }, []); // useState รันอยู่ตลอดแต่ display แสดงผลไม่ได้รับการอัปเดตถ้าไม่ได้ใช้ useEffect
 
   return (
     <div>
