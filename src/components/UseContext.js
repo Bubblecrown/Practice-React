55934b5f631c972096d85ee11e96671fedbde6f5
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-17 02:34:09 +0700

    test: UC-input&onChange

--- src/components/UseContext.js
+++ src/components/UseContext.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ThemeContext from "./ThemeContext";
 import UseContextChild from "./UseContextChild";
 // ส่งจากแม่มายังลูกโดยไม่ผ่าน prop
 // อยากให้ตัวลุกทุกตัวเข้าถึงตัวแปรของตัวแม่ได้เลย
 // ตัวแม่ provide context
 export default function UseContext() {
+  let [color, setColor] = useState({ box: "" });
+  let check = false;
   return (
     <div>
-      <ThemeContext.Provider value={"red"}>
+      <input
+        type="text"
+        onChange={(e) => {
+          setColor({ box: e.target.value }, console.log(color.box));
+        }}
+      />
+      <button
+        onClick={() => {
+          check = true;
+        }}
+      >
+        Show
+      </button>
+      <h1>{JSON.stringify(color.box)}</h1>
+      <ThemeContext.Provider value={color.box}>
         <UseContextChild />
       </ThemeContext.Provider>
     </div>
