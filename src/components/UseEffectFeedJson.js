fe8f0adf352b9dda8e6b04abe06c4ee67a82aac9
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-16 23:50:59 +0700

    finish: useEffect-feedJSON

--- src/components/UseEffectFeedJson.js
+++ src/components/UseEffectFeedJson.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 export default function UseEffectFeedJson() {
+
   const [dataArray, setDataArray] = useState(null);
+
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
       const { data } = result;
       console.log(result.data);
       // alert(JSON.stringify(result.data));
-			setDataArray(data)
+      setDataArray(data);
     });
   }, []);
 
-  return (<div><p>#debug {JSON.stringify(dataArray)}</p></div>);
+  return (
+    <div>
+      {/* <p>#debug {JSON.stringify(dataArray)}</p> */}
+      <ul>
+        {dataArray &&
+          dataArray.map(item => 
+            <li key={item.id}>{item.title}</li>
+          )}
+      </ul>
+    </div>
+  );
 }
