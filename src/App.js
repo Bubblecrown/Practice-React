54f703744e2897ea0da33eebe73d8d0b0b09a033
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-21 01:06:37 +0700

    change: folder to hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "./components/styleComponents/style.css";
-import FromComponent from "./components/FromComponent";
-import CountComponent from "./components/CountComponent";
-import UseEffect from "./components/UseEffect";
-import UseEffectInterval from "./components/UseEffectInterval";
-import UseEffectFeedJson from "./components/UseEffectFeedJson";
-import UseContext from "./components/UseContext";
-import UseMemo from "./components/UseMemo";
-import UseCallBack from "./components/UseCallBack";
+import FromComponent from "./components/hooks/FromComponent";
+import CountComponent from "./components/hooks/CountComponent";
+import UseEffect from "./components/hooks/UseEffect";
+import UseEffectInterval from "./components/hooks/UseEffectInterval";
+import UseEffectFeedJson from "./components/hooks/UseEffectFeedJson";
+import UseContext from "./components/hooks/UseContext";
+import UseMemo from "./components/hooks/UseMemo";
+import UseCallBack from "./components/hooks/UseCallBack";
 
 export default () => {
-
   return (
     <div>
       <div className="container">
